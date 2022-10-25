fc77a0ab83cfca6c7902e9cde47e327d895ee6b6
Thomas Waggoner <thomwagg@iu.edu>
2022-10-24 23:10:32 -0400

    comments and formating index

--- app.py
+++ app.py
@@ -2,18 +2,21 @@ from flask import Flask, render_template, request, redirect, url_for
 import csv
 app = Flask(__name__)
 
-# name,pet_type,level,start_date,start_time,duration
+# name,pet_type,level,start_date,start_time,duration, trainer, description
+# make a function that calls in the CSV and make a dictionary of dictionaries
 def get_courses():
     with open('courses.csv', 'r') as csvfile:
         data = csv.DictReader(csvfile)
         courseInfo = {row['name']: {'pet_type': row['pet_type'], 'level': row['level'], 'start_date': row['start_date'], 'start_time': row['start_time'], 'duration': row['duration'], 'trainer': row['trainer'], 'description': row['description']} for row in data}
     return courseInfo
 
+# make the route for the index page
 @app.route('/')
 def index():
     courseInfo = get_courses()
     return render_template('index.html', courseInfo=courseInfo, )
 
+# make a route for the course name and have the individual courses pop up when tapping on a course
 @app.route('/courses/')
 @app.route('/courses/<courseName>')
 def courses(courseName=None):
