15ebed0d2e00faa6316a5f3d800da531e3c2fce6
Thomas Waggoner <thomwagg@iu.edu>
2022-10-24 20:44:25 -0400

    reading in data into a very basic format

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+import csv
 app = Flask(__name__)
 
+# name,pet_type,level,start_date,start_time,duration
+
+with open('courses.csv', 'r') as csvfile:
+    data = csv.DictReader(csvfile)
+    courseInfo = {row['name']: {'pet_type': row['pet_type'], 'level': row['level'], 'start_date': row['start_date'], 'start_time': row['start_time'], 'duration': row['duration']} for row in data}
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html', courseInfo=courseInfo, )
+
+@app.route('/courses/')
+def courses():
+    return render_template('courses.html', courseInfo=courseInfo)
