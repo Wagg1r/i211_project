1b26851b0d3031522562728fa67b057ee9d4c050
Thomas Waggoner <thomwagg@iu.edu>
2022-10-25 23:05:37 -0400

    attempt at sorting

--- app.py
+++ app.py
@@ -3,16 +3,22 @@ from datetime import datetime
 import csv
 app = Flask(__name__)
 
-# name,pet_type,level,start_date,start_time,duration, trainer, description
-# make a function that calls in the CSV and make a dictionary of dictionaries
+# this was my attempt at making a sorted list for date it didn't run properly so i have commented it out here
 #         date = sorted(courseInfo, key = lambda item : datetime.strptime(item["start_date"],"%B %d %Y"))
+        # dateList = list(csv.reader(COURSE_PATH, delimiter=","))
+        # date = sorted(dateList, key = lambda item : datetime.strptime(item["start_date"],"%B %d %Y"))
+        # year = {'January': 1, 'Febuary': 2, 'March': 3, 'April':4, 'May': 5, 'June': 6, 'July': 7 ,'August': 8 , 'September': 9, 'October': 10, 'November':11, 'December':12}
+        # ind= 'October 19 2022'
+        # res= ind.split(' ')
+        # []
 
+        
+# make a function that calls in the CSV and make a dictionary of dictionaries
 COURSE_PATH = app.root_path + '/courses.csv'
 def get_courses():
     with open(COURSE_PATH, 'r') as csvfile:
         data = csv.DictReader(csvfile)
         courseInfo = {row['name']: {'pet_type': row['pet_type'], 'level': row['level'], 'start_date': row['start_date'], 'start_time': row['start_time'], 'duration': row['duration'], 'trainer': row['trainer'], 'description': row['description']} for row in data}
-
     return courseInfo
 get_courses()
 # make the route for the index page
